1bb99746cb867f5297413cf20b4027a7997b33b0
Natthamon Ngamsa-nga <natthamon.ng@gmail.com>
2020-03-03 13:39:21 +0100

    set up firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyADvhhT2qu9lWn3A29sgzUO9E1XINXaeAo",
+    authDomain: "meteo-yeah-weather-app.firebaseapp.com",
+    databaseURL: "https://meteo-yeah-weather-app.firebaseio.com",
+    projectId: "meteo-yeah-weather-app",
+    storageBucket: "meteo-yeah-weather-app.appspot.com",
+    messagingSenderId: "663289506781",
+    appId: "1:663289506781:web:97ec3cf9e4b3de70d4a434",
+    measurementId: "G-XEL8PHXKM8"
+  }
 };
